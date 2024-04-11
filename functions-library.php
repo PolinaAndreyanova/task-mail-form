bb5dee24da8d1ee51b78bdebff81665363b12b3e
PolinaAndreyanova <andreyanovalina@yandex.ru>
2024-04-11 19:02:54 +0000

    оптимизировать код

--- functions-library.php
+++ functions-library.php
@@ -1,4 +1,25 @@
 <?php
+function ClearValues(int $count): array
+{
+    for ($i = 0; $i < $count; $i++) {
+        $arValues[] = "";
+    }
+    return $arValues;
+}
+
+function PostDataHandler(): array
+{
+    $receiverName = $_POST["receiver-name"];
+    $receiverEmail = $_POST["receiver-email"];
+
+    $senderName = $_POST["sender-name"];
+    $senderEmail = $_POST["sender-email"];
+
+    $message = $_POST["message"];
+
+    return [$receiverName, $receiverEmail, $senderName, $senderEmail, $message];
+}
+
 function NameValidation(string $name): bool
 {
     $regex = '/^([а-яА-ЯёЁ]+)(?:$| ([а-яА-ЯёЁ]+$))/s';
@@ -11,24 +32,57 @@ function EmailValidation(string $email): bool
     return preg_match($regex, $email);
 }
 
+function HandleValidation(string $receiverName, string $receiverEmail, string $senderName, string $senderEmail, string $message): array
+{
+    $arValidationErrors = [];
+
+    if (!NameValidation($receiverName)) {
+        $arValidationErrors["receiverName"] = "Поле должно содержать имя и/или фамилию получателя кириллицей";
+    }
+
+    if (!NameValidation($senderName)) {
+        $arValidationErrors["senderName"] = "Поле должно содержать имя и/или фамилию отправителя кириллицей";
+    }
+
+    if (!EmailValidation($receiverEmail)) {
+        $arValidationErrors["receiverEmail"] = "Поле не соответствует формату email";
+    }
+
+    if (!EmailValidation($senderEmail)) {
+        $arValidationErrors["senderEmail"] = "Поле не соответствует формату email";
+    }
+
+    if (!$message) {
+        $arValidationErrors["message"] = "Сообщение не может быть пустым";
+    }
+
+    return $arValidationErrors;
+}
+
+function HandleError(string $key, array $arValidationErrors): string
+{
+    if (array_key_exists($key, $arValidationErrors)) { 
+        return $arValidationErrors[$key];
+    }
+    return "";
+}
+
 function SendMail(string $receiverName, string $mailTo, string $senderName, string $from, string $text): bool
 {
     $username = "Rti2paBWiMuH";
     $password = "H2kB2ibz5zMR";
     $host = "smtp.mailsnag.com";
-    // $from = "elvis@example.com";
     $port = "2525";
     $charset = "UTF-8";
 
     $smtp = new SendMailSmtpClass($username, $password, $host, $from, $port, $charset);
 
-    // $mailTo = "chuck@example.com";
     $site = $_SERVER['SERVER_NAME'];
     $subject = "С сайта $site отправлено сообщение";
 
     $message = "Уважаемый $receiverName, вам отправлено письмо от $senderName с сообщением:\n\n";
     $message .= $text;
-    
+
     $headers = "To: $receiverName <$mailTo>\r\nReply-To: $from\r\nContent-Type: text/plain; charset=UTF-8\r\n";
 
     return $smtp->send($mailTo, $subject, $message, $headers);
--- index.php
+++ index.php
@@ -3,54 +3,23 @@ include_once("functions-library.php");
 include_once("SendMailSmtpClass.php");
 
 $arValidationErrors = [];
-$feedback = "";
 
-$receiverName = "";
-$receiverEmail = "";
-$senderName = "";
-$senderEmail = "";
-$message = "";
+[$feedback, $receiverName, $receiverEmail, $senderName, $senderEmail, $message] = ClearValues(6);
 
 if ($_POST) {
     $feedback = "";
 
-    $receiverName = $_POST["receiver-name"];
-    $receiverEmail = $_POST["receiver-email"];
+    [$receiverName, $receiverEmail, $senderName, $senderEmail, $message] = PostDataHandler();
 
-    $senderName = $_POST["sender-name"];
-    $senderEmail = $_POST["sender-email"];
-
-    $message = $_POST["message"];
-
-    if (!NameValidation($receiverName)) {
-        $arValidationErrors["receiverName"] = "Поле должно содержать имя и/или фамилию получателя кириллицей";
-    }
-
-    if (!NameValidation($senderName)) {
-        $arValidationErrors["senderName"] = "Поле должно содержать имя и/или фамилию отправителя кириллицей";
-    }
-
-    if (!EmailValidation($receiverEmail)) {
-        $arValidationErrors["receiverEmail"] = "Поле не соответствует формату email";
-    }
-
-    if (!EmailValidation($senderEmail)) {
-        $arValidationErrors["senderEmail"] = "Поле не соответствует формату email";
-    }
-
-    if (!$message) {
-        $arValidationErrors["message"] = "Сообщение не может быть пустым";
-    }
+    $arValidationErrors = HandleValidation($receiverName, $receiverEmail, $senderName, $senderEmail, $message);
 
     if (!$arValidationErrors) {
         if (SendMail($receiverName, $receiverEmail, $senderName, $senderEmail, $message)) {
             $feedback = "Сообщение успешно отправлено!";
+
             $arValidationErrors = [];
-            $receiverName = "";
-            $receiverEmail = "";
-            $senderName = "";
-            $senderEmail = "";
-            $message = "";
+
+            [$receiverName, $receiverEmail, $senderName, $senderEmail, $message] = ClearValues(5);
         };
     }
 
@@ -69,49 +38,23 @@ if ($_POST) {
         <h1 class="content__header">Форма</h1>
 
         <h2 class="content__inputsHeader">Получатель</h2>
+        
         <input class="content__input" type="text" name="receiver-name" value="<?=$receiverName?>" placeholder="Иван" required />
-        <p class="content__feedback content__feedback_type_error">
-        <?php
-        if (array_key_exists("receiverName", $arValidationErrors)) { 
-            echo $arValidationErrors["receiverName"];
-        }
-        ?>
-        </p>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("receiverName", $arValidationErrors)?></p>
+        
         <input class="content__input" type="email" name="receiver-email" value="<?=$receiverEmail?>" placeholder="ivan85@mail.ru" required/>
-        <p class="content__feedback content__feedback_type_error">
-        <?php
-        if (array_key_exists("receiverEmail", $arValidationErrors)) { 
-            echo $arValidationErrors["receiverEmail"];
-        }
-        ?>
-        </p>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("receiverEmail", $arValidationErrors)?></p>
 
         <h2 class="content__inputsHeader">Отправитель</h2>
+        
         <input class="content__input" type="text" name="sender-name" value="<?=$senderName?>" placeholder="Александр" required/>
-        <p class="content__feedback content__feedback_type_error">
-        <?php
-        if (array_key_exists("senderName", $arValidationErrors)) { 
-            echo $arValidationErrors["senderName"];
-        }
-        ?>
-        </p>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("senderName", $arValidationErrors)?></p>
+        
         <input class="content__input" type="email" name="sender-email" value="<?=$senderEmail?>" placeholder="alex86@mail.ru" required/>
-        <p class="content__feedback content__feedback_type_error">
-        <?php
-        if (array_key_exists("senderEmail", $arValidationErrors)) { 
-            echo $arValidationErrors["senderEmail"];
-        }
-        ?>
-        </p>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("senderEmail", $arValidationErrors)?></p>
 
         <textarea class="content__textarea" name="message" placeholder="Текст сообщения" required><?=$message?></textarea>
-        <p class="content__feedback content__feedback_type_error">
-        <?php
-        if (array_key_exists("message", $arValidationErrors)) { 
-            echo $arValidationErrors["message"];
-        }
-        ?>
-        </p>
+        <p class="content__feedback content__feedback_type_error"><?=HandleError("message", $arValidationErrors)?></p>
 
         <button class="content__button" type="submit">Отправить</button>
         <p class="content__feedback content__feedback_type_success"><?=$feedback?></p>
